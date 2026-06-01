f39ceb19ca5fddb09b3862762708d582cea2b463
sakitha-s-m <s4035640@student.rmit.edu.au>
2026-06-01 10:06:43 +1000

    up

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 // Global CSS import
-import './css/bootstrap.min.css';
-import './css/font-awesome.min.css';
 import './css/style.css';
 
 // Root application component
